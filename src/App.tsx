65c0056d84dced3c22c6a6e7e4c533377f852de5
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2022-07-25 19:13:16 +0900

    使用していない関数を一旦コメントアウト

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
 import './App.css'
 import { useQRCode } from 'next-qrcode';
 
 const App = (props: any) => {
   const lineUserId = props.profile.lineUserId;
   alert(lineUserId);
-  const lineDisplayName = props.profile.lineDisplayName;
+  // const lineDisplayName = props.profile.lineDisplayName;
   const { Canvas } = useQRCode();
   return (
     <div className="App">
