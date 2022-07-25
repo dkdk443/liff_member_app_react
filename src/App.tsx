6adf2058277997f549ed2ebb964205f5c83f09a2
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2022-07-25 23:27:40 +0900

    会員情報取得を待ってQR表示

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const App = (props: any) => {
   const lineUserId = props.profile.lineUserId;
-  alert(JSON.stringify(props));
+  // alert(JSON.stringify(props));
   const [memberId, setMemberId] = useState('');
   const endpointUrl = `https://liff-member-api-laravel.herokuapp.com/api/memberId`;
   useEffect(() => {
@@ -14,37 +14,45 @@ const App = (props: any) => {
         'line_user_id': lineUserId
         }
     ).then(resp => {
-         alert(JSON.stringify(resp));
-        setMemberId(resp.data.results.member_id);
-      }).catch(error => {
-        alert(JSON.stringify(error));
-        console.log(error);
-      })
-  });
+      // alert(JSON.stringify(resp));
+      setMemberId(resp.data.results.member_id);
+    })
+    .catch(error => {
+      // alert(JSON.stringify(error));
+      console.log(error);
+    })
+  },[]);
   // const lineDisplayName = props.profile.lineDisplayName;
   const { Canvas } = useQRCode();
-  return (
-    <div className="App">
-      <div className="card">
-        <Canvas
-          text={ memberId }
-          options={{
-            type: 'image/jpeg',
-            quality: 0.3,
-            level: 'M',
-            margin: 3,
-            scale: 4,
-            width: 260,
-            color: {
-              dark: '#010599FF',
-              light: '#FFF',
-            },
-          }}
-        />
-        <p>{memberId}</p>
+  if (memberId) {
+    return (
+      <div className="App">
+        <div className="card">
+          <Canvas
+            text={ memberId ? memberId : 'ABC' }
+            options={{
+              type: 'image/jpeg',
+              quality: 0.3,
+              level: 'M',
+              margin: 3,
+              scale: 4,
+              width: 260,
+              color: {
+                dark: '#010599FF',
+                light: '#FFF',
+              },
+            }}
+          />
+          <p>{memberId}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <p className='loading'>Loading...</p>
+    );
+  }
+
 }
 
 export default App
